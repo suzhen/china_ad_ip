7ae7dabb1f1fe5ab9694a2bdc3f81e87f0daf226
suezhen <sz3001@gmail.com>
2015-03-11 13:04:38 +0800

    c language mk binarysearch

--- lib/china_ad_ip.rb
+++ lib/china_ad_ip.rb
@@ -1,12 +1,34 @@
 require "china_ad_ip/version"
 require 'china_ad_ip/ipv4'
 require 'csv'
+require 'ffi'
 
 module ChinaAdIp
   NAME            = "ChinaAdIp"
   GEM             = "china_ad_ip"
   AUTHORS         = ["suezhen <sz3001@gmail.com>"]
 
+  extend FFI::Library
+  ffi_lib "libbinarysearch.so"
+  attach_function :binarysearch, [:pointer,:long,:long], :long
+
+
+  module LibC
+      extend FFI::Library
+      ffi_lib FFI::Library::LIBC
+
+      # memory allocators
+      attach_function :malloc, [:size_t], :pointer
+      attach_function :calloc, [:size_t], :pointer
+      attach_function :valloc, [:size_t], :pointer
+      attach_function :realloc, [:pointer, :size_t], :pointer
+      attach_function :free, [:pointer], :void
+
+      # memory movers
+      attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
+      attach_function :bcopy, [:pointer, :pointer, :size_t], :void
+
+  end # module LibC
 
   def self.ntoa(uint)
     unless(uint.is_a? Numeric and uint <= 0xffffffff and uint >= 0)
@@ -64,9 +86,7 @@ module ChinaAdIp
     ip_addr = ChinaAdIp::IPv4.new(addr)
 
     if $ip_sections && $locations
-
-      index = dichotomy(ip_addr.to_i,$ip_sections.collect{|ip_section| ip_section[0]}.to_a) 
-
+      index =  ChinaAdIp.binarysearch(self.arr_pointer,ip_addr.to_i,ip_arr.length)
       $locations[$ip_sections[index][2]]
     else
       raise(::ArgumentError, "not exists csv data")    
@@ -75,16 +95,13 @@ module ChinaAdIp
   end
 
 
-  def self.dichotomy(search,array)
-    index = 0 , start_index = 0 , end_index = array.length
-    while(true)
-      index = start_index + ((end_index - start_index) / 2)
-      start_index = index if array[index] < search
-      end_index = index if array[index] > search
-      break if array[index] == search || ((start_index == index) && (start_index+1 == end_index))
-    end 
-    index
+  def self.ip_arr
+    @@ip_arr ||= $ip_sections.collect{|ip_section| ip_section[0]}.to_a
   end
 
+  def self.arr_pointer
+    buffer = LibC.malloc(ip_arr.first.size * ip_arr.size)
+    buffer.write_array_of_long ip_arr
+  end
 
 end
--- lib/china_ad_ip/version.rb
+++ lib/china_ad_ip/version.rb
@@ -1,3 +1,3 @@
 module ChinaAdIp
-  VERSION = "0.0.3"
+  VERSION = "0.1.0"
 end
